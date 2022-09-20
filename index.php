2eaf095fdfba90a7eb924534961856f449b35f56
Albiddd <alberto.dore93@gmail.com>
2022-09-20 15:18:41 +0200

    aggiunta form

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-    $text = 'Tutto lavoro e niente svago rendono Jack un ragazzo noioso';
+    $text = 'Tutto lavoro e niente svago rendono Jack un ragazzo noioso.';
     $length = strlen($text);
     $w = $_GET['word'];
     $newString = str_replace($w, '***', $text, $count);
     $newLength = strlen($newString);
 ?>
 
-<h2>Testo:</h2>
+<h2>Vecchio testo:</h2>
 <p><?= $text ?></p>
 <p>Il testo è lungo: <?= $length ?> caratteri.</p>
+
 <h2>Testo censurato:</h2>
 <p> <?= $newString ?> </p>
 <p>Ora il testo è lungo: <?= $newLength ?> caratteri.</p>
